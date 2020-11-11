aacd134883d3d276c2111235839e8d360e879102
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2020-11-11 20:42:52 +0100

    simple App with local state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import SignupStyles from './css/SignupAttendee.module.scss';
+import AttendeeListStyles from './css/AttendeeList.module.scss';
+type email = string;
 
-function App() {
+interface Attendee {
+  name: string;
+  email: email;
+  rsvp: boolean;
+}
+
+function AttendeeList({ attendees }: { attendees: Attendee[] }) {
+  return (
+    <ul className={AttendeeListStyles.ul}>
+      {attendees.map(
+        (attendee: Attendee, idx: number) => (
+          <li key={`attendee-${idx}`}>
+            <p>
+              <b>{attendee.name}</b><br />
+              <i>{attendee.email}</i><br />
+              <span>{attendee.rsvp ? "coming" : "skipping"}</span>
+            </p>
+          </li>
+        ))}
+    </ul >
+  )
+}
+
+const SignupAttendee = ({ onAdded }: { onAdded: (attendee: Attendee) => void }) => {
+
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [rsvp, setRsvp] = useState<boolean>(false);
+
+  const submit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const attendee: Attendee = {
+      name, email, rsvp
+    }
+    onAdded(attendee);
+    setName(""); setEmail(""); setRsvp(false)
+  }
+
+  return <form onSubmit={submit} className={SignupStyles.form}>
+    <div>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+    </div>
+    <div>
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    </div>
+    <div>
+      <label htmlFor="rsvp">I&apos;m coming!</label>
+      <input name="rsvp" type="checkbox" checked={rsvp} onClick={() => setRsvp(!rsvp)} />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+
+}
+
+const App: React.FC = () => {
+
+  const [attendees, setAttendees] = useState([{
+    name: "Stefan",
+    email: "stadolf@gmail.com",
+    rsvp: true
+  }]);
+
+  const addAttendee = (attendee: Attendee) => {
+    setAttendees([...attendees, attendee]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <AttendeeList attendees={attendees} />
+        <SignupAttendee onAdded={addAttendee} />
+      </main>
     </div>
   );
 }
